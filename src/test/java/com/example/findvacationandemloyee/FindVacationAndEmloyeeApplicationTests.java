d0484d258622dec01690082505fe6322109bd94e
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-07-18 18:19:46 +0300

    initial commit

---
+++ src/test/java/com/example/findvacationandemloyee/FindVacationAndEmloyeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findvacationandemloyee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindVacationAndEmloyeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

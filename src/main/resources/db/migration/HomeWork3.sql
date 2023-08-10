63cd7f828ead94a043976af78449022b86128cae
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-08-10 19:43:20 +0300

    complete homework № 3

--- src/main/resources/db/migration/HomeWork3.sql
+++ src/main/resources/db/migration/HomeWork3.sql
@@ -5,8 +5,63 @@ FROM orders
 WHERE ship_country LIKE 'U%';
 
 --2--
-SELECT order_id,customer_id,freight,ship_country
+SELECT order_id, customer_id, freight, ship_country
 FROM orders
 WHERE ship_country LIKE 'N%'
 ORDER BY freight DESC
 LIMIT 10;
+
+--3--
+SELECT first_name, last_name, home_phone, region
+FROM employees
+WHERE region ISNULL;
+
+--4--
+SELECT count(*)
+FROM customers
+WHERE region IS NOT NULL;
+
+--5--
+SELECT country, count(*)
+FROM suppliers
+GROUP BY country
+ORDER BY count(*) DESC;
+
+
+--6--
+SELECT ship_country, sum(freight)
+FROM orders
+WHERE ship_region IS NOT NULL
+GROUP BY ship_country
+HAVING sum(freight) > 2750
+ORDER BY sum(freight) DESC;
+
+--7--
+SELECT DISTINCT country
+FROM customers
+UNION
+SELECT DISTINCT country
+FROM suppliers
+ORDER BY country;
+
+--8--
+SELECT DISTINCT country
+FROM customers
+INTERSECT
+SELECT DISTINCT country
+FROM suppliers
+INTERSECT
+SELECT country
+FROM employees;
+
+--9--
+SELECT country
+FROM customers
+INTERSECT
+SELECT country
+FROM suppliers
+EXCEPT
+SELECT country
+FROM employees;
+
+
